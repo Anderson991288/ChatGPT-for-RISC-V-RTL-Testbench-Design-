c7be2d46ed25ce28959add71407dd389229796c4
Anderson991288 <68816726+Anderson991288@users.noreply.github.com>
2023-02-27 22:18:40 +0800

    Add files via upload

---
+++ code/rom_tb.v
@@ -0,0 +1,28 @@
+module rom_tb;
+    reg clk;
+    reg [4:0] addr;
+    wire [31:0] data_out;
+
+    rom dut (
+        .clk(clk),
+        .addr(addr),
+        .data_out(data_out)
+    );
+
+    initial begin
+        clk = 0;
+        addr = 0;
+        #10; // wait 10 units of time
+        repeat(16) begin // iterate over all addresses
+            addr = addr + 1;
+            #10; // wait another 10 units of time
+        end
+        $finish; // stop simulation
+    end
+
+    always #5 clk = ~clk; // toggle clock every 5 units of time
+
+    always @(posedge clk) begin
+        $display("Address %d contains data : %h", addr, data_out);
+    end
+endmodule
